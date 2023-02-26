ec174a232fab3f1d484457e8e3e4ef11067598da
Alexander Matthew <alexanderm61@gmail.com>
2023-02-26 21:31:10 +0700

    tmp fix: remove this shit temporarily
    I don't understand what's this for. don't notice any diff

--- coq/lsp/requests/command.py
+++ coq/lsp/requests/command.py
@@ -1,17 +1,19 @@
-from std2.pickle.encoder import new_encoder
+#FIX ME: breaking elixir-ls and other lsp
+#from std2.pickle.encoder import new_encoder
 
 from ...shared.types import ExternLSP, ExternLUA
-from ..types import Command
-from .request import async_request
+#from ..types import Command
+#from .request import async_request
 
-_ENCODER = new_encoder[Command](Command)
+#_ENCODER = new_encoder[Command](Command)
 
 
 async def cmd(extern: ExternLSP) -> None:
-    if extern.command:
-        name = "lsp_third_party_cmd" if isinstance(extern, ExternLUA) else "lsp_command"
-        command = _ENCODER(extern.command)
+    return
+    #if extern.command:
+    #    name = "lsp_third_party_cmd" if isinstance(extern, ExternLUA) else "lsp_command"
+    #    command = _ENCODER(extern.command)
 
-        clients = {extern.client} if extern.client else set()
-        async for _ in async_request(name, clients, command):
-            pass
+    #    clients = {extern.client} if extern.client else set()
+    #    async for _ in async_request(name, clients, command):
+    #        pass
